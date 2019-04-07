23e697cdad4244cbb282884bfa7bc589c560f607
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-04-07 14:13:50 -0500

    added option module and tests

--- src/test.js
+++ src/test.js
@@ -1,15 +1,20 @@
 (() => {
   // runtime imports
+  const Option = ModOption;
   const User = ModUser;
 
   console.log("Running test.js");
 
-  console.assert(User != undefined);
-  console.assert(User.with_pass("loremipsum") != undefined);
+  // Option Tests
+  {
+    console.assert(Option !== undefined);
+    console.assert(Option.some !== undefined);
+    console.assert(Option.none !== undefined);
 
-  let u = User.with_pass("loremipsum");
-  console.log(u);
-
-  console.log(User);
+    var obj = Option.some("String");
+    console.assert(obj.has_some());
+    console.assert(!obj.has_none());
+    console.assert(obj.unwrap() === "String");
+  }
 
 })();
