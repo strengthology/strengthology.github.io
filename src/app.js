7d0e9b428d595237eca4e36d2e0d676fb4d26e56
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-07-12 17:06:19 -0500

    Added vue

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 import {User} from './models.js'
 import {UserController} from './controllers.js'
 
+/*
+ASCI builder functions
+*/
+const charMul = (char, num) => {
+  let r = '';
+  for(let i = 0; i < num; i++){
+    r += char;
+  }
+  return r;
+}
+
+/* TODO:
+Steps:
+  - load the ui (async)
+  - connect to the db (async)
+*/
+
+/*
+Database Connection
+*/
 if(!window.indexedDB){
   console.log("Your browser does not support indexedDB.");
 }
@@ -9,12 +29,28 @@ const idb = window.indexedDB;
 
 let db;
 let req = idb.open("testdb");
+
 req.onerror = (event) => {
   console.log("Error.")
 };
+
 req.onsuccess = (event) => {
   console.log("Success");
   db = event.target.result;
   console.log(db);
 };
+
 console.log(idb);
+
+/*
+UI Behavior
+*/
+const title = ".__[ Strengthology ]__.";
+
+let view1 = new Vue({
+  el: '#nav',
+  data: {
+    header: title,
+    art: charMul('- ', title.length - 2) + '-',
+  }
+}));
