3f15b4fb8849d29d4350c233b53bbfe2988fb367
James Meyer <jamesjmeyer210@gmail.com>
2020-01-11 11:41:41 -0600

    better run messages

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# to run:
-# $sh run.sh
 
-if [ "$(whereis static-server)" != "static server:\n" ] ; then
-  static-server --ip 127.0.0.1 -p 7878
+# look for our static file server
+if [ "$(whereis simple-http-server)" != "simple-http-server:\n" ] ; then
+  simple-http-server --ip 127.0.0.1 \
+  	--port 8000 \
+  	--nocache \
+  	--index
+else
+# it cannot be found so recommend installing it
+	println "%s\n%s\n" \
+		"Error: simple-http-server not installed." \
+		"Run $ cargo install simple-http-server"
 fi
